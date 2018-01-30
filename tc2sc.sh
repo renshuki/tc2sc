f43904fbca5bf0e74c65ca20f6ba0c8f9f82bacb
renshuki <renshuki@mail.com>
2018-01-30 12:15:54 +0900

    Initial commit

---
+++ tc2sc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+convert_data="tc2sc.csv"
+
+while IFS=',' read -r tc_char sc_char
+do
+  echo "Converting $tc_char -> $sc_char"
+  sed -i "s/${tc_char}/${sc_char}/g" $1;
+done < "$convert_data"
